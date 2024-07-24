fdab7825d503e41cb80bd1ca460fc15253c1f7b8
maeve-teaching <maeve.rse.teaching@gmail.com>
2024-07-24 10:51:01 +0000

    Add pseudo code

---
+++ src/hypot/calc.py
@@ -0,0 +1,13 @@
+# we want:
+# h = sqrt(opp^2 + adj^2)
+# we are going to use pure python
+
+# calc the square of the value
+
+
+# calc the addition of two values
+
+
+# calc the squareroot of a value
+
+# calc the hypot
